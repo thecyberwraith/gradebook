bc50897fee0ebd4d57f4492581e4ce260359a839
Michael Lugo <michaelrlugo@gmail.com>
2018-01-04 11:15:55 -0500

    Integrated SafeDictionary with Student
    With a SafeDictionary, now students can easily and safely be loaded
    to and from dictionaries.

--- student.py
+++ student.py
@@ -1,15 +1,73 @@
+from enum import Enum
 import logging
 
-from enum import Enum
+from csv_io import SafeDictionary, make_safe
 
-class StudentStatus(Enum):
+class StoredEnum(Enum):
+	'''
+	Subclasses of this Enum should have upper case enum items. Then loading and
+	saving to a dictionary is handled by this class instead of children
+	classes. Assumes one value of child is UNDEFINED=0
+	'''
+
+	@classmethod
+	@make_safe
+	def from_dictionary(cls, safe_dictionary):
+		key = cls.get_key()
+		if key in safe_dictionary and safe_dictionary[key] != '':
+			enum_name = safe_dictionary[key].upper()
+			# To ignore honors, try this
+			enum_name = enum_name.split(' ')[0]
+			
+			try:
+				return cls[enum_name]
+			except KeyError:
+				raise KeyError('Value {} is not a valid {}'.format(
+					enum_name,
+					cls
+				))
+
+			return cls[safe_dictionary[key].upper()]
+		else:
+			return cls.UNDEFINED
+	
+	def to_dictionary(self, dictionary):
+		if self.value == 0: # Undefined
+			raise ValueError('Cannot save an undefined value.')
+		dictionary[self.__class__.get_key()] = self.name
+	
+	@classmethod
+	def get_key(cls):
+		'''
+		Overridden to provide class specific keys.
+		'''
+		return None
+	
+class StudentStatus(StoredEnum):
+	UNDEFINED = 0
 	ACTIVE = 1
 	WITHDRAWN = 2
 	DROPPED = 3
 
-class StudentName(object):
-	keys = ['Last Name', 'First Name', 'Preferred Name']
+	@classmethod
+	def get_key(self):
+		return 'Status'
 
+class StudentYear(StoredEnum):
+	UNDEFINED = 0
+	FRESHMAN = 1
+	SOPHOMORE = 2
+	JUNIOR = 3
+	SENIOR = 4 
+
+	@classmethod
+	def get_key(self):
+		return 'Year'
+
+class StudentName(object):
+	keys = ['Last Name', 'First Name', 'Preferred']
+
+	@make_safe
 	def __init__(self, first, last, preferred=None):
 		self._first = first
 		self._last = last
@@ -50,22 +108,32 @@ class StudentName(object):
 		else:
 			return self.first_last
 	
+	def __str__(self):
+		return self.expanded
+
+	def __repr__(self):
+		return 'StudentName({})'.format(self)
+
 	@staticmethod
-	def from_dictionary(dictionary):
-		if not (StudentName.keys[0] in dictionary and StudentName.keys[1] in dictionary):
-			logging.debug('About to fail at dictionary {}'.format(dictionary))
-			raise KeyError('No name could be extracted from the information.')
+	@make_safe
+	def from_dictionary(safe_dictionary):
+		keys = StudentName.keys
+		if not (keys[0] in safe_dictionary and keys[1] in safe_dictionary):
+			logging.debug('About to fail at dictionary {}'.format(safe_dictionary))
+			raise KeyError('No name could be extracted from the dictionary {}'.format(safe_dictionary))
 
-		preferred_name = ''
+		preferred_name = None
 
-		if StudentName.keys[2] in dictionary:
-			preferred_name = dictionary[StudentName.keys[2]]
+		if keys[2] in safe_dictionary:
+			preferred_name = safe_dictionary[keys[2]]
 
 		name = StudentName(
-			first = dictionary[StudentName.keys[0]],
-			last = dictionary[StudentName.keys[1]],
+			first = safe_dictionary[keys[0]],
+			last = safe_dictionary[keys[1]],
 			preferred = preferred_name
 		)
+		if name is None:
+			print(name)
 		return name
 	
 	def to_dictionary(self, dictionary):
@@ -80,11 +148,12 @@ class Student(object):
 	'''
 
 	'''
-	The keys required from a dictionary. Also used to export to csv.
+	The keys required from a dictionary. Also used to export to csv. Not case sensitive
 	'''
-	keys = ['Student ID', 'Email', 'Year', 'Status', 'Major'] + StudentName.keys
+	keys = ['Student ID', 'Email', 'Major']
 
-	def __init__(self, data):
+	@make_safe
+	def __init__(self, safe_dictionary):
 		'''
 		Given a dictionary of data, create a student with the specified
 		information. This data should contain the following keys:
@@ -98,11 +167,21 @@ class Student(object):
 			Status
 			Major
 		'''
+		self._name = StudentName.from_dictionary(safe_dictionary)
+		self._status = StudentStatus.from_dictionary(safe_dictionary)
+		self._year = StudentYear.from_dictionary(safe_dictionary)
 
 		for key in Student.keys:
-			safe_key = '_{}'.format(key.replace(' ', '_').lower())
-			setattr(self, safe_key, data[key])
-		self._name = StudentName.from_dictionary(data)
+			if key in safe_dictionary:
+				safe_key = SafeDictionary.make_key_safe(key)
+				attribute_name = '_{}'.format(safe_key)
+				setattr(self, attribute_name, safe_dictionary[key])
+			else:
+				raise KeyError('Student dictionary missing {}. Must have list {}, got list {}'.format(
+					key,
+					Student.keys,
+					list(safe_dictionary)
+				))
 	
 	@property
 	def name(self):
@@ -126,6 +205,8 @@ class Student(object):
 	
 	@status.setter
 	def status(self, status):
+		if not isinstance(status, StudentStatus):
+			raise ValueError('Status must be an instance of the enum StudentStatus')
 		self._status = status
 	
 	@property
@@ -147,7 +228,11 @@ class Student(object):
 		'''
 		data = dict()
 		for key in Student.keys:
-			data[key] = getattr(self, '_{}'.format(key.replace(' ', '_')).lower())
+			safe_key = SafeDictionary.make_key_safe(key)
+			data[key] = getattr(self, safe_key)
+		self.status.to_dictionary(data)
+		self.year.to_dictionary(data)
+		self.name.to_dictionary(data)
 
 		return data
 
@@ -155,4 +240,4 @@ class Student(object):
 		return self.name.expanded
 	
 	def __repr__(self):
-		return str(self)
+		return 'Student({})'.format(str(self))
--- tests/test_student.py
+++ tests/test_student.py
@@ -1,48 +1,133 @@
-import unittest
+from unittest import TestCase
+from student import *
 
-import student
-
-class TestStudentName(unittest.TestCase):
+class TestStudentName(TestCase):
 	def test_constructor_without_preferred_name(self):
-		name = student.StudentName('bob', 'joe')
+		name = StudentName('bob', 'joe')
 		self.assertEqual('bob', name.first)
 		self.assertEqual('joe', name.last)
 		self.assertEqual(None, name._preferred)
 	
 	def test_constructor_with_preferred_name(self):
-		name = student.StudentName('bob', 'joe', 'susie')
+		name = StudentName('bob', 'joe', 'susie')
 		self.assertEqual('bob', name.first)
 		self.assertEqual('joe', name.last)
 		self.assertEqual('susie', name._preferred)
 	
 	def test_property_first_last(self):
-		name = student.StudentName('bob', 'joe', 'susie')
+		name = StudentName('bob', 'joe', 'susie')
 		self.assertEqual('bob joe', name.first_last)
 
 	def test_property_last_first(self):
-		name = student.StudentName('bob', 'joe')
+		name = StudentName('bob', 'joe')
 		self.assertEqual('joe, bob', name.last_first)
 
 	def test_property_preferred_without_preferred(self):
-		name = student.StudentName('bob', 'joe')
+		name = StudentName('bob', 'joe')
 		self.assertEqual('bob joe', name.preferred)
 	
 	def test_property_preferred_with_preferred(self):
-		name = student.StudentName('bob', 'joe', 'susie')
+		name = StudentName('bob', 'joe', 'susie')
 		self.assertEqual('susie joe', name.preferred)
 	
 	def test_property_expanded_without_preferred(self):
-		name = student.StudentName('bob', 'joe')
+		name = StudentName('bob', 'joe')
 		self.assertEqual('bob joe', name.expanded)
 	
 	def test_property_expanded_with_preferred(self):
-		name = student.StudentName('bob', 'joe', 'susie')
+		name = StudentName('bob', 'joe', 'susie')
 		self.assertEqual('bob (susie) joe', name.expanded)
 	
 	def test_staticmethod_from_dictionary(self):
 		name_components = ['bob', 'joe', 'susie']
-		dictionary = {student.StudentName.keys[i]: name_components[i] for i in range(3)}
-		name = student.StudentName.from_dictionary(dictionary)
+		dictionary = {StudentName.keys[i]: name_components[i] for i in range(3)}
+		name = StudentName.from_dictionary(dictionary)
 		self.assertEqual('bob', name.first)
 		self.assertEqual('joe', name.last)
 		self.assertEqual('susie', name._preferred)
+
+	def test_method_to_dictionary(self):
+		name_components = ['bob', 'joe', 'susie']
+		name_dict = {StudentName.keys[i]: name_components[i] for i in range(3)}
+		name = StudentName.from_dictionary(name_dict)
+		result = dict()
+		name.to_dictionary(result)
+		self.assertEqual(name_dict, result)
+
+class TestStoredEnums(TestCase):
+	def test_from_dictionary_status(self):
+		self.assertEqual(
+			StudentStatus.ACTIVE,
+			StudentStatus.from_dictionary({StudentStatus.get_key(): 'Active'})
+		)
+	
+	def test_to_dictionary(self):
+		result = dict()
+		StudentStatus.DROPPED.to_dictionary(result)
+		self.assertEqual(result, {StudentStatus.get_key(): 'DROPPED'})
+	
+	def test_error_on_save_undefined(self):
+		with self.assertRaises(ValueError):
+			StudentStatus.UNDEFINED.to_dictionary(dict())
+		with self.assertRaises(ValueError):
+			StudentYear.UNDEFINED.to_dictionary(dict())
+
+class TestStudent(TestCase):
+	def setUp(self):
+		self.sample_data = {
+			'Student id': 5,
+			'First name': 'bob',
+			'Last name': 'joe',
+			'Preferred': 'susie',
+			'Email': 'bjoe@com.net',
+			'Year': 'Sophomore',
+			'Major': 'Ice Cream Technician',
+			'Status': 'Active',
+		}
+
+	def test_constructor(self):
+		Student(self.sample_data)
+	
+	def test_constructor_fails_with_insufficient_data(self):
+		data = self.sample_data.copy()
+		del data['Student id']
+		with self.assertRaises(KeyError):
+			Student(data)
+	
+	def test_constructor_fails_with_bad_year(self):
+		data = self.sample_data.copy()
+		data['Year'] = 'old'
+		with self.assertRaises(KeyError):
+			Student(data)
+	
+	def test_constructor_fails_with_bad_status(self):
+		data = self.sample_data.copy()
+		data['Status'] = 'Homeschooled'
+		with self.assertRaises(KeyError):
+			Student(data)
+	
+	def test_set_values(self):
+		student = Student(self.sample_data)
+		self.assertEqual(student.student_id, self.sample_data['Student id'])
+		self.assertEqual(student.email, self.sample_data['Email'])
+		self.assertEqual(student.major, self.sample_data['Major'])
+		self.assertEqual(student.year, StudentYear.SOPHOMORE)
+		self.assertEqual(student.status, StudentStatus.ACTIVE)
+	
+	def test_pid(self):
+		student = Student(self.sample_data)
+		self.assertEqual(student.pid, 'bjoe')
+	
+	def test_to_dictionary(self):
+		student = Student(self.sample_data)
+		result = student.to_dictionary()
+	
+	def test_setter_status(self):
+		student = Student(self.sample_data)
+		self.assertEqual(student.status, StudentStatus.ACTIVE)
+	
+	def test_setter_status_type(self):
+		student = Student(self.sample_data)
+		with self.assertRaises(ValueError):
+			student.status = 'WITHDRAWN'
+		student.status = StudentStatus.WITHDRAWN

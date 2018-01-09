125a14a5e022f786b9b4df6ab99d5868c1c5b65b
Michael Lugo <michaelrlugo@gmail.com>
2018-01-09 11:47:13 -0500

    WebAssign has updated their roster requirements.

--- generate_webassign_roster.py
+++ generate_webassign_roster.py
@@ -12,12 +12,25 @@ def generate_roster(args):
 
 	with open(config.OUTPUT_WA_ROSTER_PATH, 'w') as f:
 		writer = csv.writer(f)
-		writer.writerow(['Name', 'Username', 'Email', 'Role'])
+		writer.writerow(['firstname', 'lastname', 'email', 'password'])
 
 		for student in students:
 			writer.writerow([
-				student.last_first_name,
-				student.pid,
+				student.first_name,
+				student.last_name,
 				student.email,
-				'Student'
+				make_password(student)
 			])
+
+def make_password(student):
+	'''
+	Min length 6
+	Max length 17
+	Must have a capital letter and a number
+	'''
+	email = student.email
+	if len(email) > 16:
+		email = email[:16]
+	
+	return email.upper() + ''.join((17-len(email)) * ['0'])
+
